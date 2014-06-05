4900b8549177137e5ceb86aa587ab4cb702ac2cb
DamonHao <haolinknight@gmail.com>
2014-06-05 21:39:34 +0800

    add loop and fd

--- libev-cpp/libev_cpp_example.cc
+++ libev-cpp/libev_cpp_example.cc
@@ -6,16 +6,18 @@
  */
 
 #include <ev++.h>
-
 #include <stdio.h>
+
+#define BUFFERSIZE 50
 
 class MyClass
 {
 public:
-	MyClass()
+	MyClass(ev::loop_ref loop, int fd) :
+			io_watcher_(loop)
 	{
 		io_watcher_.set<MyClass, &MyClass::io_cb>(this);
-		io_watcher_.set(STDIN_FILENO, ev::READ);
+		io_watcher_.set(fd, ev::READ);
 	}
 	void start()
 	{
@@ -24,7 +26,16 @@ public:
 private:
 	void io_cb(ev::io &io_watcher, int revents)
 	{
-		puts("io cb\n");
+		puts("in io_cb");
+		char buf[BUFFERSIZE];
+		if(gets(buf) != NULL)
+		{
+			puts(buf);
+		}
+		else
+		{
+			puts("read fd error");
+		}
 		io_watcher.stop();
 	}
 	ev::io io_watcher_;
@@ -33,7 +44,7 @@ private:
 int main()
 {
 	ev::default_loop loop;
-	MyClass my_io;
+	MyClass my_io(loop, STDIN_FILENO);
 	my_io.start();
 	loop.run();
 	return 0;
