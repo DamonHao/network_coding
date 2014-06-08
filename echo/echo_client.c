901e394314e3930bd671c04b17425eeffaabb1bc
DamonHao <haolinknight@gmail.com>
2014-06-08 21:37:18 +0800

    add input

--- echo/echo_client.c
+++ echo/echo_client.c
@@ -21,10 +21,14 @@
 
 int main(int agrc, char *argv[])
 {
+	if (agrc != 3)
+	{
+		puts("usage:<program> <server ip> <port>");
+		exit(0);
+	}
 	puts("echo client up!");
-	const char *server_ip = "127.0.0.1";
-	int server_port = 9999;
-	char input_buff[MAXBUF];
+	const char *server_ip = argv[1];
+	int server_port = atoi(argv[2]);
 	int sockfd;
 	//Create streaming socket
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -37,19 +41,46 @@ int main(int agrc, char *argv[])
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(server_port);
 	inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
+
 	if (connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr))
 			!= 0)
 	{
 		perror("connect");
 		exit(errno);
 	}
-	char *message = "echo echo";
-	int send_num = send(sockfd, message, strlen(message), 0);
-	printf("send num:%d\n", send_num);
-	int recv_num = recv(sockfd, input_buff, sizeof(input_buff), 0);
-	printf("recv num:%d\n", recv_num);
+
+	char output_buff[MAXBUF];
+	char input_buff[MAXBUF];
+	while (fgets(output_buff, MAXBUF, stdin) != NULL)
+	{
+		int send_num = send(sockfd, output_buff, strlen(output_buff) + 1, 0);
+		if (send_num <= 0)
+		{
+			perror("socket send");
+			exit(errno);
+		}
+		int recv_num = 0;
+		int recv_count = 0;
+		while ((recv_num = recv(sockfd, input_buff, sizeof(input_buff), 0)) > 0)
+		{
+			printf("%s", input_buff);
+			recv_count += recv_num;
+			if (recv_count == send_num)
+			{
+				break;
+			}
+		}
+		if (recv_num < 0)
+		{
+			perror("socket recv");
+			exit(errno);
+		}
+		else if (recv_num == 0)
+		{
+			break;
+		}
+	}
 	close(sockfd);
-	puts(input_buff);
 	return 0;
 }
 
